3ff95fb06c7656870a6cd4c7f3e16ec91ae29330
Алена Климшина <118774450+Aliona2006@users.noreply.github.com>
2023-02-13 14:58:36 +0300

    Add files via upload

--- interactive map/interactive map/Form1.Designer.cs
+++ interactive map/interactive map/Form1.Designer.cs
@@ -53,6 +53,8 @@
             this.pictureBox10 = new System.Windows.Forms.PictureBox();
             this.pictureBox9 = new System.Windows.Forms.PictureBox();
             this.pictureBox8 = new System.Windows.Forms.PictureBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
@@ -132,10 +134,10 @@
             // label7
             // 
             this.label7.AutoSize = true;
-            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.label7.Location = new System.Drawing.Point(626, 88);
             this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(183, 42);
+            this.label7.Size = new System.Drawing.Size(117, 29);
             this.label7.TabIndex = 12;
             this.label7.Text = "gvbfgnfnn";
             this.label7.Visible = false;
@@ -304,6 +306,7 @@
             this.pictureBox10.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox10.TabIndex = 15;
             this.pictureBox10.TabStop = false;
+            this.pictureBox10.Click += new System.EventHandler(this.pictureBox10_Click);
             // 
             // pictureBox9
             // 
@@ -314,6 +317,7 @@
             this.pictureBox9.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox9.TabIndex = 14;
             this.pictureBox9.TabStop = false;
+            this.pictureBox9.Click += new System.EventHandler(this.pictureBox9_Click);
             // 
             // pictureBox8
             // 
@@ -324,13 +328,39 @@
             this.pictureBox8.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox8.TabIndex = 13;
             this.pictureBox8.TabStop = false;
+            this.pictureBox8.Click += new System.EventHandler(this.pictureBox8_Click);
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label8.Location = new System.Drawing.Point(619, 76);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(93, 33);
+            this.label8.TabIndex = 16;
+            this.label8.Text = "label8";
+            this.label8.Visible = false;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label9.Location = new System.Drawing.Point(594, 193);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(70, 25);
+            this.label9.TabIndex = 17;
+            this.label9.Text = "label9";
+            this.label9.Visible = false;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
             this.BackColor = System.Drawing.SystemColors.Window;
-            this.ClientSize = new System.Drawing.Size(989, 615);
+            this.ClientSize = new System.Drawing.Size(1305, 615);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
             this.Controls.Add(this.pictureBox10);
             this.Controls.Add(this.pictureBox9);
             this.Controls.Add(this.pictureBox8);
@@ -358,6 +388,7 @@
             this.Controls.Add(this.pictureBox1);
             this.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
             this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Marathon Skills 2016 – Interactive map";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
@@ -401,6 +432,8 @@
         private System.Windows.Forms.PictureBox pictureBox10;
         private System.Windows.Forms.PictureBox pictureBox9;
         private System.Windows.Forms.PictureBox pictureBox8;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
     }
 }
 
--- interactive map/interactive map/Form1.cs
+++ interactive map/interactive map/Form1.cs
@@ -31,8 +31,8 @@ namespace interactive_map
             label5.Visible = false;
             label7.Visible = true;
             label7.Text = "МЦК Лужники";
-            pictureBox6.Location = new Point(633, 516);
-            label5.Location = new Point(748, 549);
+            pictureBox6.Location = new Point(316, 268);
+            label5.Location = new Point(374, 285);
         }
 
         private void button5_Click_1(object sender, EventArgs e)
@@ -49,8 +49,8 @@ namespace interactive_map
             label5.Visible = false;
             label7.Visible = true;
             label7.Text = "Парк Горького";
-            pictureBox6.Location = new Point(633, 516);
-            label5.Location = new Point(748, 549);
+            pictureBox6.Location = new Point(316, 268);
+            label5.Location = new Point(374, 285);
         }
 
         private void button4_Click_1(object sender, EventArgs e)
@@ -67,8 +67,8 @@ namespace interactive_map
             label5.Visible = true;
             label7.Visible = true;
             label7.Text = "МИД";
-            pictureBox6.Location = new Point(633, 423);
-            label5.Location = new Point(748, 453);
+            pictureBox6.Location = new Point(316, 220);
+            label5.Location = new Point(374, 236);
         }
 
         private void button2_Click(object sender, EventArgs e)
@@ -85,8 +85,8 @@ namespace interactive_map
             label5.Visible = true;
             label7.Visible = true;
             label7.Text = "Новодевичей монастырь";
-            pictureBox6.Location = new Point(633, 516);
-            label5.Location = new Point(748, 549);
+            pictureBox6.Location = new Point(316, 268);
+            label5.Location = new Point(374, 285);
         }
 
         private void button3_Click(object sender, EventArgs e)
@@ -103,8 +103,8 @@ namespace interactive_map
             label5.Visible = false;
             label7.Visible = true;
             label7.Text = "Метро Киевская";
-            pictureBox6.Location = new Point(633, 516);
-            label5.Location = new Point(748, 549);
+            pictureBox6.Location = new Point(316, 268);
+            label5.Location = new Point(374, 285);
         }
 
         private void button6_Click(object sender, EventArgs e)
@@ -121,8 +121,8 @@ namespace interactive_map
             label5.Visible = false;
             label7.Visible = true;
             label7.Text = "Здания РАН";
-            pictureBox6.Location = new Point(633, 516);
-            label5.Location = new Point(748, 549);
+            pictureBox6.Location = new Point(316, 268);
+            label5.Location = new Point(374, 285);
         }
 
         private void button7_Click(object sender, EventArgs e)
@@ -139,8 +139,8 @@ namespace interactive_map
             label5.Visible = true;
             label7.Visible = true;
             label7.Text = "Метро Воробевы горы";
-            pictureBox6.Location = new Point(633, 516);
-            label5.Location = new Point(748, 549);
+            pictureBox6.Location = new Point(316, 268);
+            label5.Location = new Point(374, 285);
         }
 
         private void button8_Click(object sender, EventArgs e)
@@ -157,8 +157,68 @@ namespace interactive_map
             label5.Visible = true;
             label7.Visible = true;
             label7.Text = "Стадион Лужники";
-            pictureBox6.Location = new Point(633, 516);
-            label5.Location = new Point(748, 549);
+            pictureBox6.Location = new Point(316, 268);
+            label5.Location = new Point(374, 285);
+        }
+
+        private void pictureBox8_Click(object sender, EventArgs e)
+        {
+            pictureBox2.Visible = false;
+            pictureBox3.Visible = false;
+            pictureBox4.Visible = false;
+            pictureBox5.Visible = false;
+            pictureBox6.Visible = false;
+            label1.Visible = false;
+            label2.Visible = false;
+            label3.Visible = false;
+            label4.Visible = false;
+            label5.Visible = false;
+            label7.Visible = false;
+            label8.Visible = true;
+            label9.Visible = true;
+            label8.Text = "Старт марафона";
+            label9.Text = "Полный маршрут:" +
+                "40 километров";
+        }
+
+        private void pictureBox9_Click(object sender, EventArgs e)
+        {
+            pictureBox2.Visible = false;
+            pictureBox3.Visible = false;
+            pictureBox4.Visible = false;
+            pictureBox5.Visible = false;
+            pictureBox6.Visible = false;
+            label1.Visible = false;
+            label2.Visible = false;
+            label3.Visible = false;
+            label4.Visible = false;
+            label5.Visible = false;
+            label7.Visible = false;
+            label8.Visible = true;
+            label9.Visible = true;
+            label8.Text = "Старт марафона";
+            label9.Text = "Часть маршрута:" +
+                "20 километров";
+        }
+
+        private void pictureBox10_Click(object sender, EventArgs e)
+        {
+            pictureBox2.Visible = false;
+            pictureBox3.Visible = false;
+            pictureBox4.Visible = false;
+            pictureBox5.Visible = false;
+            pictureBox6.Visible = false;
+            label1.Visible = false;
+            label2.Visible = false;
+            label3.Visible = false;
+            label4.Visible = false;
+            label5.Visible = false;
+            label7.Visible = false;
+            label8.Visible = true;
+            label9.Visible = true;
+            label8.Text = "Старт марафона";
+            label9.Text = "Часть маршрута:" +
+                "5 километров";
         }
     }
 }
