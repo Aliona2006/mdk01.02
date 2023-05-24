312e4c3dc4a3220f035cb6092f106f1cb6d7554b
Алена Климшина <118774450+Aliona2006@users.noreply.github.com>
2023-05-24 22:48:51 +0300

    Add files via upload

---
+++ PR1_V13_Klimshina24IS-21/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PR1_V13_Klimshina24IS_21
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double itog = Math.Round(((2.7 - 0.8) * 2 * (1.0 / 3.0)) / ((5.2 - 1.4) / Math.Pow(3.0 / 7.0, 1.0 / 5.0)) - Math.Pow(2.625, 1.0 / 5.0) + 8 * (9.0 / 25.0) - ((95 + 7.0 / 30.0 - 93 * (5.0 / 18.0)) * 2 * (1.0 / 4.0)) / Math.Sqrt(2) + Math.Pow(3.14, 1.0 / 6.0));
+            Console.WriteLine("Результат вычислений: " + itog);
+        }
+    }
+}
