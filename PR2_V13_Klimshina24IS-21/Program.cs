a45bc03d173b6c8e336962d4e62d9132d402fe44
Алена Климшина <118774450+Aliona2006@users.noreply.github.com>
2023-05-24 22:49:23 +0300

    Add files via upload

---
+++ PR2_V13_Klimshina24IS-21/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PR2_V13_Klimshina24IS_21
+{
+    internal class Program
+    {
+        //Даны две прямые. Определить,перпендикулярны ли они.
+
+
+        static void Main(string[] args)
+        {
+            Console.Write("Введите коэффициенты первой прямой: ");
+            double k1 = Convert.ToDouble(Console.ReadLine());
+            double b1= Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Введите коэффициенты второй прямой: ");
+            double k2 = Convert.ToDouble(Console.ReadLine());
+            double b2 = Convert.ToDouble(Console.ReadLine());
+
+            if (k1 * k2 == -1.0)
+            {
+                Console.WriteLine("Прямые перпендикулярны.");
+            }
+            else
+            {
+                Console.WriteLine("Прямые не перпендикулярны.");
+            }
+
+        }
+    }
+}
