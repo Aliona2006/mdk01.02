e36064f697a465f8571c2b17968fb1d66e17f96c
Алена Климшина <118774450+Aliona2006@users.noreply.github.com>
2023-05-24 22:48:05 +0300

    Add files via upload

---
+++ LB1_V13_Klimshina24IS-21/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LB1_V13_Klimshina24IS_21
+{
+    internal class Program
+    {
+        //Проверить соотношение log(a,b)=ln(a)/ln(b)
+        static void Main(string[] args)
+        {
+            // Запросить у пользователя значение a, b
+            Console.Write("Введите значение a: ");
+            double a = double.Parse(Console.ReadLine());
+            Console.Write("Введите значение b: ");
+            double b = double.Parse(Console.ReadLine());
+
+            // Вычислить соотношение
+            double log = (Math.Log(a)) / (Math.Log(b));
+            
+
+            // Вывести результаты на экран
+            Console.WriteLine("log({0}, {1}) = {2}", a, b, log);
+        }
+    }
+}
