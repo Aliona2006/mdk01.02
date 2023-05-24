4b7cb480565f3b72aedd0a284970aeaa0003a381
Алена Климшина <118774450+Aliona2006@users.noreply.github.com>
2023-05-24 22:53:34 +0300

    Add files via upload

---
+++ PR5_V13_Klimshina24IS-21/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PR5_V13_Klimshina24IS_21
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
